9a5ec3a0d0823435e3f09e55512fd838c924750b
huseyncode <huseynmsh@code.edu.az>
2024-11-05 04:46:02 +0400

    first commit

---
+++ ZayShop/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZayShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
