616dc405e65f867235aa0003ddea3819b4bd54e6
huseyncode <huseynmsh@code.edu.az>
2024-11-13 11:54:45 +0400

    Slider CRUD finished

---
+++ ZayShop/Areas/Admin/Models/Slider/SliderUpdateVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZayShop.Areas.Admin.Models.Slider;
+
+public class SliderUpdateVM
+{
+    [Required(ErrorMessage = "Header1 is required.")]
+    [MinLength(3, ErrorMessage = "Header 1 must be at least 3 characters long.")]
+    public string Header1 { get; set; }
+
+    [Required(ErrorMessage = "Header 2 is required.")]
+    [MinLength(3, ErrorMessage = "Header2 must be at least 3 characters long.")]
+    public string Header2 { get; set; }
+
+    [Required(ErrorMessage = "Description is required.")]
+    [MinLength(10, ErrorMessage = "Description must be at least 10 characters long.")]
+    public string Description { get; set; }
+
+    public string PhotoPath { get; set; }
+
+}
